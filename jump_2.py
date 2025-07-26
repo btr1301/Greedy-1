1b289ef867405ac0ec2b505c5b0c1fb7dac28dcc
BTR <thilakraj1301@gmail.com>
2025-07-25 19:12:15 -0700

    Create jump_2.py

---
+++ jump_2.py
@@ -0,0 +1,13 @@
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+def jump(nums):
+    jumps = 0
+    current_end = 0
+    farthest = 0
+    for i in range(len(nums) - 1):
+        farthest = max(farthest, i + nums[i])
+        if i == current_end:
+            jumps += 1
+            current_end = farthest
+    return jumps
